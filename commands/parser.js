6ea50447c576260a55860ea0c4f378a2ea5da62c
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-02-26 20:03:40 -0500

    Update parser.js

--- commands/parser.js
+++ commands/parser.js
@@ -2,7 +2,68 @@ import message_send from "../../functions/messagecreate.js";
 import axios from "axios";
 import fetch from 'node-fetch';
 import cheerio from "cheerio";
+import responsehandler from "./responsehandling.js";
 import message_send_embed from "../../functions/messagecreateembed.js";
+
+function parsehtmlPath(parentitem) {
+    var parseparentitem;
+                    if (parentitem.startsWith("div")) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `div.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `div#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'div';
+                    } else if (parentitem.startsWith("p")) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `p.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `p#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'p';
+                    } else if (parentitem.startsWith("span")) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `span.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `span#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'span';
+                    } else if (parentitem.indexOf("header") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `h1.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `h1#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'h1';
+                    } else if (parentitem.indexOf("table") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `table.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `table#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'table';
+                    } else if (parentitem.indexOf("tr") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `tr.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `tr#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'tr';
+                    } else if (parentitem.indexOf("td") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `td.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `td#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'td';
+                    } else if (parentitem.indexOf("a") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `a.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `a#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'a';
+                    } else if (parentitem.indexOf("aside") > -1) {
+                        if (parentitem.indexOf("&") > -1) {
+                            parseparentitem = `aside.` + parentitem.split("&")[1].replace(/_/g, ".");
+                        } else if (parentitem.indexOf("#") > -1) {
+                            parseparentitem = `aside#` + parentitem.split("#")[1].replace(/_/g, ".");
+                        } else parseparentitem = 'aside';
+                    } else message_send(location, "BehemothBotParser: Invalid HTML tag");
+                    return parseparentitem;
+}
 
 const reqHandlerParser = async (location, args) => {
     try {
@@ -56,28 +117,21 @@ const reqHandlerParser = async (location, args) => {
                         body: JSON.stringify(content),          
                     });
                     const response = await fetchPOSTrequest.json();
-                    if (args.indexOf("<rawresponse>") > -1) message_send(location, JSON.stringify(response));
-                   await message_send(location, response);                 
+                    if (args.indexOf("<rawresponse>") > -1) {
+                    responsehandler(JSON.stringify(response), args, location);
+                    }                
                 } else message_send(location, "Error: Malformed request, when forming a POST request make sure to include the headers and content");
             }
             break;
         case "GET":
             const response = await axios.get(args[1]);
             if (args.indexOf("<rawresponse>") > -1) { 
-                if (args.indexOf("<formatfordiscordF>") > -1) {
-                    if (response.data.length <= 2000 && response.data.length > 0) message_send(location, 'BehemothBotParser: Unnecessary discord formatting');
-                    if (args.indexOf('<limit:') > -1) message_send(location, 'BehemothBotParser: Error, <limit> cannot be used with <formatfordiscord> as they conflict');
-                    else message_send(location, response.data.substring(0, 2000)); 
-                } else if (args.indexOf("<limit:") > -1) {
-                    const position = args.indexOf('<limit:') + 1;
-                    const limitedvalue = args[position].replace(">", "");
-                    if (limitedvalue > 2000) message_send(location, "BehemothBotParser: Unnecessary limit value, discord char limit is 2000");
-                    if (limitedvalue == 0) message_send(location, "BehemothBotParser: Unnecessary limit value assignment, Cannot send 0 characters");
-                    if (limitedvalue < 0) message_send(location, "BehemothBotParser: Error, illogical limit value"); else {
-                    message_send(location, response.data.substring(0, limitedvalue));
-                    }
-                } else if (!args.indexOf("<formatfordiscordF>") && !args.indexOf("<limit:")) message_send(location, response.data);
-            } else if (args.indexOf("=>") > -1) {
+                if (args.indexOf("<defaultF>") > -1) {
+                message_send(location, response.data)
+            } else if (args.indexOf("<jumptohandler>") > -1) {
+                responsehandler(response.data, args, location);
+            }
+            } else if (args.indexOf("=>") > -1 && args.indexOf("<jumptohandler>") <= -1) {
                 const parsing = args.slice(args.indexOf("=>") + 1, args.length);
                 if (!parsing.length) message_send(location, "BehemothBotParser: Error, no parsing orders exist");
                 const $ = cheerio.load(response.data);
@@ -93,25 +147,49 @@ const reqHandlerParser = async (location, args) => {
                                 items.push(`div.` + temp[i].split("&")[1].replace(/_/g, "."));
                             } else if (temp[i].indexOf("#") > -1) {
                                 items.push(`div#` + temp[i].split("#")[1].replace(/_/g, "."))
-                            }
+                            } else (items.push(`div`));
                         } else if (temp[i].startsWith("p")) {
                             if (temp[i].indexOf("&") > -1) {
                                 items.push(`p.` + temp[i].split("&")[1].replace(/_/g, "."));
                             } else if (temp[i].indexOf("#") > -1) {
                                 items.push(`p#` + temp[i].split("#")[1].replace(/_/g, "."))
-                            }
+                            } else (items.push(`p`));
                         } else if (temp[i].startsWith("span")) {
                             if (temp[i].indexOf("&") > -1) {
                                 items.push(`span.` + temp[i].split("&")[1].replace(/_/g, "."));
                             } else if (temp[i].indexOf("#") > -1) {
                                 items.push(`span#` + temp[i].split("#")[1].replace(/_/g, "."))
-                            }
+                            } else (items.push(`span`));
                         } else if (temp[i].indexOf("header") > -1) {
                             if (temp[i].indexOf("&") > -1) {
                                 items.push(`h1.` + temp[i].split("&")[1].replace(/_/g, "."));
                             } else if (temp[i].indexOf("#") > -1) {
                                 items.push(`h1#` + temp[i].split("#")[1].replace(/_/g, "."))
-                            }
+                            } else (items.push(`h1`));
+                        } else if (temp[i].indexOf("table") > -1) {
+                            if (temp[i].indexOf("&") > -1) {
+                                items.push(`table.` + temp[i].split("&")[1].replace(/_/g, "."));
+                            } else if (temp[i].indexOf("#") > -1) {
+                                items.push(`table#` + temp[i].split("#")[1].replace(/_/g, "."))
+                            } else (items.push(`table`));
+                        } else if (temp[i].indexOf("tr") > -1) {
+                            if (temp[i].indexOf("&") > -1) {
+                                items.push(`tr.` + temp[i].split("&")[1].replace(/_/g, "."));
+                            } else if (temp[i].indexOf("#") > -1) {
+                                items.push(`tr#` + temp[i].split("#")[1].replace(/_/g, "."))
+                            } else (items.push(`tr`));
+                        } else if (temp[i].indexOf("td") > -1) {
+                            if (temp[i].indexOf("&") > -1) {
+                                items.push(`td.` + temp[i].split("&")[1].replace(/_/g, "."));
+                            } else if (temp[i].indexOf("#") > -1) {
+                                items.push(`td#` + temp[i].split("#")[1].replace(/_/g, "."))
+                            } else (items.push(`td`));
+                        } else if (temp[i].indexOf("a") > -1) {
+                            if (temp[i].indexOf("&") > -1) {
+                                items.push(`a.` + temp[i].split("&")[1].replace(/_/g, "."));
+                            } else if (temp[i].indexOf("#") > -1) {
+                                items.push(`a#` + temp[i].split("#")[1].replace(/_/g, "."))
+                            } else (items.push(`a`));
                         }
                     }
                     if (parsing.indexOf("<class:") > -1) {
@@ -121,37 +199,11 @@ const reqHandlerParser = async (location, args) => {
                     } else items.push(sendorder);
                     const newitems = items.join(" > ");
                     const newinfo = $(newitems).text();
-                    console.log(newinfo);
+                    responsehandler(newinfo, parsing, location);
                 } else if (parsing.indexOf("<parent:") > -1) {
                     const pos = parsing.indexOf('<parent:') + 1;
 				    const parentitem = parsing[pos].replace(">", "");
-                    var parseparentitem;
-                    var reqitem;
-                    if (parentitem.startsWith("div")) {
-                        if (parentitem.indexOf("&") > -1) {
-                            parseparentitem = `div.` + parentitem.split("&")[1].replace(/_/g, ".");
-                        } else if (parentitem.indexOf("#") > -1) {
-                            parseparentitem = `div#` + parentitem.split("#")[1].replace(/_/g, ".");
-                        }
-                    } else if (parentitem.startsWith("p")) {
-                        if (parentitem.indexOf("&") > -1) {
-                            parseparentitem = `p.` + parentitem.split("&")[1].replace(/_/g, ".");
-                        } else if (parentitem.indexOf("#") > -1) {
-                            parseparentitem = `p#` + parentitem.split("#")[1].replace(/_/g, ".");
-                        }
-                    } else if (parentitem.startsWith("span")) {
-                        if (parentitem.indexOf("&") > -1) {
-                            parseparentitem = `span.` + parentitem.split("&")[1].replace(/_/g, ".");
-                        } else if (parentitem.indexOf("#") > -1) {
-                            parseparentitem = `span#` + parentitem.split("#")[1].replace(/_/g, ".");
-                        }
-                    } else if (parentitem.indexOf("header") > -1) {
-                        if (parentitem.indexOf("&") > -1) {
-                            parseparentitem = `h1.` + parentitem.split("&")[1].replace(/_/g, ".");
-                        } else if (parentitem.indexOf("#") > -1) {
-                            parseparentitem = `h1#` + parentitem.split("#")[1].replace(/_/g, ".");
-                        }
-                    }
+                    var parseparentitem = parsehtmlPath(parentitem);
                     if (parsing.indexOf("<class:") > -1) {
                         const pos2 = parsing.indexOf('<class:') + 1;
 				        const classval2 = parsing[pos2].replace(">", "");
@@ -162,8 +214,8 @@ const reqHandlerParser = async (location, args) => {
                         const pos3 = parsing.indexOf('<child:') + 1;
 				        const childvalue = parsing[pos3].replace(">", "");
                         const buildchildrenrequest = `${parseparentitem} ${sendorder}:nth-child(${childvalue})`
-                        console.log($(buildchildrenrequest).text());
-                    } else console.log($(buildrequest).text());
+                        responsehander($(buildchildrenrequest).text(), parsing, location);
+                    } else responsehandler($(buildrequest).text(), parsing, location);
                 }
                 break;  
             }              
@@ -173,34 +225,8 @@ const reqHandlerParser = async (location, args) => {
                     const $ = cheerio.load(response.data);
                     const pos = parsing.indexOf('<fulltag:') + 1;
 				    const tagval = parsing[pos].replace(">", "");
-                    var parseparentitem;
-                    var reqitem;
-                    if (tagval.startsWith("div")) {
-                        if (tagval.indexOf("&") > -1) {
-                            parseparentitem = `div.` + tagval.split("&")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") > -1) {
-                            parseparentitem = `div#` + tagval.split("#")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") <= -1 && tagval.indexOf("&") <= -1) parseparentitem = 'div';
-                    } else if (tagval.startsWith("p")) {
-                        if (tagval.indexOf("&") > -1) {
-                            parseparentitem = `p.` + tagval.split("&")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") > -1) {
-                            parseparentitem = `p#` + tagval.split("#")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") <= -1 && tagval.indexOf("&") <= -1) parseparentitem = 'p';
-                    } else if (parentitem.startsWith("span")) {
-                        if (tagval.indexOf("&") > -1) {
-                            parseparentitem = `span.` + tagval.split("&")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") > -1) {
-                            parseparentitem = `span#` + tagval.split("#")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") <= -1 && tagval.indexOf("&") <= -1) parseparentitem = 'span';
-                    } else if (tagval.indexOf("header") > -1) {
-                        if (tagval.indexOf("&") > -1) {
-                            parseparentitem = `h1.` + tagval.split("&")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") > -1) {
-                            parseparentitem = `h1#` + tagval.split("#")[1].replace(/_/g, ".");
-                        } else if (tagval.indexOf("#") <= -1 && tagval.indexOf("&") <= -1) parseparentitem = 'h1';
-                    }
-                    console.log($(parseparentitem).text());
+                    var parseparentitem = parsehtmlPath(tagval);
+                    responsehandler($(parseparentitem).text(), parsing, location);
                 }   
         break;
         default:
